7175158aa1816dd4653ccac7fa2d12265f7a4a72
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-01-31 23:24:16 +0900

    [FIX] How to Get Binary Data

--- Study_WebFlux/src/main/java/com/test/tester/controller/TesterController.java
+++ Study_WebFlux/src/main/java/com/test/tester/controller/TesterController.java
@@ -1,9 +1,17 @@
 package com.test.tester.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.tester.domain.TestDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.buffer.*;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import org.springframework.core.io.buffer.DataBuffer;
+
 @Slf4j
 @RestController
 public class TesterController {
@@ -22,6 +30,43 @@ public class TesterController {
         String msg2 = m.block();
         log.info("pos2 " + msg2);
         return m;
+    }
+    public Mono<TestDTO> getBodyAsTestDTO(Flux<DataBuffer> body) {
+        return DataBufferUtils.join((Flux<DataBuffer>) body)
+                .map(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        return objectMapper.readValue(bytes, TestDTO.class);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    @PostMapping("/test")
+    public Mono<Void> handleRequest(ServerWebExchange serverWebExchange) {
+        ServerHttpRequest serverHttpRequest = serverWebExchange.getRequest();
+        Flux<DataBuffer> body = serverHttpRequest.getBody();
+        return DataBufferUtils.join(body)
+                .flatMap(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+                    return Mono.just(bytes);
+                })
+                .flatMap(res -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        TestDTO testDTO = objectMapper.readValue(res, TestDTO.class);
+                        log.info(testDTO.getX() + " " + serverHttpRequest.getHeaders().get("TEST"));
+                        return Mono.empty();
+                    } catch (Exception e) {
+                        return Mono.error(e);
+                    }
+                });
     }
 
     private String generateHello() {
---
+++ Study_WebFlux/src/main/java/com/test/tester/domain/TestDTO.java
@@ -0,0 +1,9 @@
+package com.test.tester.domain;
+
+import lombok.Data;
+
+@Data
+public class TestDTO {
+    int x;
+    int y;
+}
